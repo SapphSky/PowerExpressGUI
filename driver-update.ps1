00e77822ebae3d7420f688325fd3213fc3b99a38
Joel Fargas <joelfargas@revivn.com>
2024-07-05 08:57:17 -0400

    Changes: PSWindowsUpdate install logic

--- driver-update.ps1
+++ driver-update.ps1
@@ -9,12 +9,16 @@ if (Get-Module -Name PSWindowsUpdate) {
 }
 else {
     Write-Host 'Installing PSWindowsUpdate...';
-    Install-PackageProvider -Name NuGet -Force;
+    Install-PackageProvider -Name NuGet -Force | Out-Null;
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
     Install-Module -Name PSWindowsUpdate -Force;
-    Start-Sleep -Seconds 1;
 
-    if (Import-Module PSWindowsUpdate -Force) {
+    if (!Get-Module -Name PSWindowsUpdate) {
+        Write-Error 'Error: Could not get PSWindowsUpdate module.';
+        return;
+    }
+    
+    if (Import-Module -Name PSWindowsUpdate -Force) {
         Write-Host 'Extraction Successful.';
         CheckForUpdates;
     }
