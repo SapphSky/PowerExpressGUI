8b674462b17ac474885e5befbe740cdf7815a681
Joel Fargas <joelfargas@revivn.com>
2024-07-05 10:38:30 -0400

    ScheduledTask does not work, let's try schtasks

--- preboot.ps1
+++ preboot.ps1
@@ -1,17 +1,23 @@
 $Url = "https://github.com/SapphSky/PowerExpressGUI/raw/main/driver-update.ps1";
 $FilePath = "C:\PowerExpressGUI\autorun.ps1";
-# $schtasksCommand = 'schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr powershell.exe /ru System /mo ONLOGON /z /rl HIGHEST /delay 0000:10'
 
 # Download the script
-Write-Progress -Activity "Bootstrapping PowerExpressGUI" -Status "Downloading script";
 Invoke-RestMethod $Url -OutFile $FilePath;
-Write-Progress -Activity "Bootstrapping PowerExpressGUI" -Status "Registering Scheduled Task";
+
+if (Test-Path -Path $FilePath) {
+    Write-Host "Download completed.";
+}
+else {
+    Write-Host "Error: $FilePath not found. Aborting.";
+}
+Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
 
 # Creates a Scheduled Task to run our script at startup
-$Action = New-ScheduledTaskAction -Execute $FilePath;
-$Trigger = New-ScheduledTaskTrigger -AtLogon;
-$Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
-Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
+# $Action = New-ScheduledTaskAction -Execute $FilePath;
+# $Trigger = New-ScheduledTaskTrigger -AtLogon;
+# $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
+# Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
+schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr $FilePath /ru System /mo ONLOGON /z /rl HIGHEST /delay 0000:10;
 
 Write-Progress -Activity "Bootstrapping PowerExpressGUI" -Status "Completed";
 Start-Sleep -Seconds 1;
