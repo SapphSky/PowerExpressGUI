47a5a9d76f351c45a4e490b18a31e46df2458c38
Joel Fargas <joelfargas@revivn.com>
2024-07-05 09:45:49 -0400

    Simplify user

--- preboot.ps1
+++ preboot.ps1
@@ -4,9 +4,7 @@
 
 # Creates a Scheduled Task to run our script at startup
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command 'irm https://github.com/SapphSky/PowerExpressGUI/raw/main/driver-update.ps1 | iex";
-$Trigger = New-ScheduledTaskTrigger -AtLogon  -RandomDelay (New-TimeSpan -Minutes 1) -User "defaultuser0";
-$Principal = New-ScheduledTaskPrincipal -UserId "$($env:USERDOMAIN)\defaultuser0" -LogonType ServiceAccount
+$Trigger = New-ScheduledTaskTrigger -AtLogon -User "$env:USERDOMAIN\defaultuser0";
 $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
 
-Register-ScheduledTask -TaskName "PowerExpressGUI" -TaskPath "\PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -Force;
-
+Register-ScheduledTask -TaskName "PowerExpressGUI" -TaskPath "\PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -Force;
