c001f3e1669fdaff84c53c7339543cc274fa89ba
Joel Fargas <joelfargas@revivn.com>
2024-07-05 12:03:49 -0400

    Please work...

--- driver-update.ps1
+++ driver-update.ps1
@@ -1,22 +1,24 @@
-$MaxAttempts = 5;
+$Command = {
+    $MaxAttempts = 5;
 
-for ($i = 1; $i -le $MaxAttempts; $i++) {
-    if (-Not (Get-PackageProvider -Name Nuget)) {
-        Install-PackageProvider -Name NuGet -Force;
+    for ($i = 1; $i -le $MaxAttempts; $i++) {
+        if (-Not (Get-PackageProvider -Name Nuget)) {
+            Install-PackageProvider -Name NuGet -Force;
+        }
+        if (-Not (Get-PSRepository -Name PSGallery)) {
+            Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
+        }
+        if (-Not (Get-Module -Name PSWindowsUpdate)) {
+            Install-Module -Name PSWindowsUpdate -Force;
+            Start-Sleep -Seconds 1;
+            Import-Module -Name PSWindowsUpdate -Force;
+        }
+        else {
+            Install-WindowsUpdate -AcceptAll -UpdateType Driver;
+            break;
+        }
+        $i++;
     }
-    if (-Not (Get-PSRepository -Name PSGallery)) {
-        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
-    }
-    if (-Not (Get-Module -Name PSWindowsUpdate)) {
-        Install-Module -Name PSWindowsUpdate -Force;
-        Start-Sleep -Seconds 1;
-        Import-Module -Name PSWindowsUpdate -Force;
-    }
-    else {
-        Write-Host 'Checking for driver updates...';
-        Install-WindowsUpdate -AcceptAll -UpdateType Driver;
-        Write-Host 'Driver updates completed. Computer will now automatically restart (if necessary)';
-        break;
-    }
-    $i++;
 }
+
+Start-Process powershell -Verb RunAs -ArgumentList "-Command (Invoke-Command -ScriptBlock {$Command})";
--- preboot.ps1
+++ preboot.ps1
@@ -1,28 +1,7 @@
 $Url = "https://github.com/SapphSky/PowerExpressGUI/raw/main/driver-update.ps1";
 $FilePath = "C:\PowerExpressGUI\autorun.ps1";
 
-# Download the script
-Invoke-RestMethod $Url -OutFile $FilePath;
-
-if (Test-Path -Path $FilePath) {
-    Write-Host "Download completed.";
-}
-else {
-    Write-Host "Error: $FilePath not found.";
-}
-Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
-
-# Creates a Scheduled Task to run our script at startup
-# $Action = New-ScheduledTaskAction -Execute $FilePath;
-# $Trigger = New-ScheduledTaskTrigger -AtLogon;
-# $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
-# Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
-schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr "powershell.exe -NoLogo -NoExit -File $FilePath" /ru System /mo ONLOGON /z /rl HIGHEST /delay 0000:10;
-
-Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Completed";
-Start-Sleep -Seconds 1;
-
-[xml]$Task = @'
+[xml]$ScheduledTaskXML = @'
 <?xml version="1.0" encoding="UTF-16"?>
 <Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
   <RegistrationInfo>
@@ -72,9 +51,27 @@ Start-Sleep -Seconds 1;
   </Settings>
   <Actions Context="Author">
     <Exec>
-      <Command>"powershell.exe"</Command>
-      <Argument>"-File C:\PowerExpressGUI\autorun.ps1"</Argument>
+      <Command>"cmd"</Command>
+      <Argument>"powershell -File C:\PowerExpressGUI\autorun.ps1"</Argument>
     </Exec>
   </Actions>
 </Task>
 '@
+
+# Download the script
+Invoke-RestMethod -Uri $Url -OutFile $FilePath;
+
+Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
+Register-ScheduledTask -Xml $ScheduledTaskXML;
+
+# schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr "powershell.exe -NoLogo -NoExit -File $FilePath" /ru System /mo ONLOGON /z /rl HIGHEST /delay 0000:10;
+
+# Creates a Scheduled Task to run our script at startup
+# $Action = New-ScheduledTaskAction -Execute $FilePath;
+# $Trigger = New-ScheduledTaskTrigger -AtLogon;
+# $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
+# Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
+
+
+Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Completed";
+Start-Sleep -Seconds 1;
