d9ca40c423270b1634ea9828c652e82a4b58320c
Joel Fargas <joelfargas@revivn.com>
2024-07-05 10:43:26 -0400

    schtasks edit

--- preboot.ps1
+++ preboot.ps1
@@ -17,7 +17,7 @@ Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Sch
 # $Trigger = New-ScheduledTaskTrigger -AtLogon;
 # $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
 # Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
-schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr $FilePath /ru System /mo ONLOGON /z /rl HIGHEST /delay 0000:10;
+schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr "powershell.exe -NoLogo -NoExit -File $FilePath" /ru System /mo ONLOGON /z /rl HIGHEST /delay 0000:10;
 
 Write-Progress -Activity "Bootstrapping PowerExpressGUI" -Status "Completed";
 Start-Sleep -Seconds 1;
