349e42af1d7cfc5dec84be93d275df0149f42e66
Joel Fargas <joelfargas@revivn.com>
2024-07-05 12:54:47 -0400

    Debugging

--- preboot.ps1
+++ preboot.ps1
@@ -2,11 +2,6 @@ if (-Not (Test-Path "C:\PowerExpressGUI\")) {
     New-Item -Path "C:\" -Name "PowerExpressGUI" -ItemType "directory";
 }
 
-# Download the Task Scheduler XML file
-$TaskXmlUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/task.xml";
-$TaskXmlFile = "C:\PowerExpressGUI\task.xml";
-Invoke-RestMethod $TaskXmlUrl -OutFile $TaskXmlFile;
-
 # Download the autorun script
 $AutorunUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/driver-update.ps1";
 $AutorunFile = "C:\PowerExpressGUI\autorun.ps1";
@@ -16,7 +11,7 @@ if ((Test-Path $TaskXmlFile) -and (Test-Path $AutorunFile)) {
     Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
 
     # Creates a Scheduled Task to run our script at startup
-    $Action = New-ScheduledTaskAction -Execute $FilePath;
+    $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-File $AutorunFile";
     $Trigger = New-ScheduledTaskTrigger -AtLogon;
     $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
     Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
@@ -24,3 +19,5 @@ if ((Test-Path $TaskXmlFile) -and (Test-Path $AutorunFile)) {
 
 Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Completed";
 Start-Sleep -Seconds 1;
+
+Start-Process powershell -ArgumentList "-NoExit"
