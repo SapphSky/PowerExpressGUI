03fdcd0fae56c06e96a4ba5b782a9ef330b6c124
Joel Fargas <joelfargas@revivn.com>
2024-07-05 15:38:29 -0400

    Just realized the glaring issue

--- preboot.ps1
+++ preboot.ps1
@@ -1,38 +1,26 @@
 if (-Not (Test-Path "C:\PowerExpressGUI\")) {
+    Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Creating directory";
     New-Item -Path "C:\" -Name "PowerExpressGUI" -ItemType "directory";
 }
 
 # Download the autorun script
-$AutorunUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/driver-update.ps1";
+$AutorunUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/content/driver-update.ps1";
 $AutorunFile = "C:\PowerExpressGUI\autorun.ps1";
 Invoke-RestMethod -Uri $AutorunUrl -OutFile $AutorunFile;
 
-if ((Test-Path $TaskXmlFile) -and (Test-Path $AutorunFile)) {
-    Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
+# $TaskUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/content/task.xml";
+# $TaskFile = "C:\PowerExpressGUI\task.xml";
+# Invoke-RestMethod -Uri $TaskUrl -OutFile $TaskFile;
 
-    # Creates a Scheduled Task to run our script at startup
-    $Action = New-ScheduledTaskAction `
-        -Execute "powershell" `
-        -Argument "Start-Process powershell -Verb RunAs -WindowStyle Normal -File $AutorunFile";
+Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
 
-    $Trigger = New-ScheduledTaskTrigger `
-        -AtLogon;
+# Creates a Scheduled Task to run our script at startup
+$Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-Verb RunAs -File $AutorunFile";
+$Trigger = New-ScheduledTaskTrigger -AtLogon;
+$Principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest;
 
-    $Principal = New-ScheduledTaskPrincipal `
-        -GroupId "BUILTIN\Administrators" `
-        -RunLevel Highest;
-    
-    Register-ScheduledTask `
-        -TaskName "PowerExpressGUI" `
-        -Description "From SapphSky/PowerExpressGUI" `
-        -Action $Action `
-        -Trigger $Trigger `
-        -Principal $Principal;
-
-    $Task = Get-ScheduledTask -TaskName "PowerExpressGUI";
-    $TaskInfo = Get-ScheduledTaskInfo -InputObject $Task;
-    Write-Host $TaskInfo;
-}
+Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Principal $Principal;
+# Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Xml (Get-Content $TaskFile);
 
 Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Completed";
 Start-Sleep -Seconds 1;
