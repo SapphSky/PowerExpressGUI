930ef4353c2b8163164fe66fc581cdb133ceb009
Joel Fargas <joelfargas@revivn.com>
2024-07-05 10:45:27 -0400

    schtasks edit

--- preboot.ps1
+++ preboot.ps1
@@ -8,7 +8,7 @@ if (Test-Path -Path $FilePath) {
     Write-Host "Download completed.";
 }
 else {
-    Write-Host "Error: $FilePath not found. Aborting.";
+    Write-Host "Error: $FilePath not found.";
 }
 Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
 
@@ -19,5 +19,5 @@ Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Sch
 # Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
 schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr "powershell.exe -NoLogo -NoExit -File $FilePath" /ru System /mo ONLOGON /z /rl HIGHEST /delay 0000:10;
 
-Write-Progress -Activity "Bootstrapping PowerExpressGUI" -Status "Completed";
+Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Completed";
 Start-Sleep -Seconds 1;
