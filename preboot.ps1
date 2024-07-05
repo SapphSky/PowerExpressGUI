573652fc393cf4122b69e44803e18f57ec028931
Joel Fargas <joelfargas@revivn.com>
2024-07-05 12:51:16 -0400

    Can't do xml because account names and security id mapping is required

--- preboot.ps1
+++ preboot.ps1
@@ -1,4 +1,6 @@
-New-Item -Path "C:\" -Name "PowerExpressGUI" -ItemType "directory"
+if (-Not (Test-Path "C:\PowerExpressGUI\")) {
+    New-Item -Path "C:\" -Name "PowerExpressGUI" -ItemType "directory";
+}
 
 # Download the Task Scheduler XML file
 $TaskXmlUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/task.xml";
@@ -12,18 +14,13 @@ Invoke-RestMethod -Uri $AutorunUrl -OutFile $AutorunFile;
 
 if ((Test-Path $TaskXmlFile) -and (Test-Path $AutorunFile)) {
     Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Registering Scheduled Task";
-    schtasks.exe /Create /XML $TaskXmlFile /tn "PowerExpressGUI - Auto Install Drivers";
+
+    # Creates a Scheduled Task to run our script at startup
+    $Action = New-ScheduledTaskAction -Execute $FilePath;
+    $Trigger = New-ScheduledTaskTrigger -AtLogon;
+    $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
+    Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
 }
 
 Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Completed";
 Start-Sleep -Seconds 1;
-
-# Disabled code, please ignore
-# Register-ScheduledTask -TaskName "PowerExpressGUI" -Xml $ScheduledTaskXML;
-# schtasks /create /sc ONLOGON /tn "powerexpressgui\Install Drivers" /tr "powershell.exe -NoLogo -NoExit -File $FilePath" /ru System /z /rl HIGHEST /delay 0000:10;
-
-# Creates a Scheduled Task to run our script at startup
-# $Action = New-ScheduledTaskAction -Execute $FilePath;
-# $Trigger = New-ScheduledTaskTrigger -AtLogon;
-# $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Days 1);
-# Register-ScheduledTask -TaskName "PowerExpressGUI" -Description "From SapphSky/PowerExpressGUI" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest;
