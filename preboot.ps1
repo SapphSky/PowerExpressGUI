8c9c566d7b1c9daa6d778d2aa3b4eba8e7c60a28
Joel Fargas <joelfargas@revivn.com>
2024-07-08 12:09:05 -0400

    ngh

--- preboot.ps1
+++ preboot.ps1
@@ -31,7 +31,7 @@ Register-ScheduledTask `
     -Principal $Principal `
     -Settings $Settings `
     -Trigger $Trigger `
-    -Verbose | Out-File $SetupPath\createtask.txt
+    -Verbose | Out-File "$SetupPath\createtask.txt"
 
 $Task = Get-ScheduledTask -TaskName $TaskName;
 
@@ -48,5 +48,5 @@ if ($Task) {
 }
 else {
     Write-Progress -Activity $ProgressTitle -Status "Error: Failed to register task.";
-    Start-Process "powershell" -Verb RunAs -Wait -ArgumentList "-NoExit -Command 'echo Looks like PowerExpressGUI ran into an error. You can use this terminal to see what went wrong, or close and continue your installation like normal.'";
+    Start-Process "powershell" -Verb RunAs -Wait -ArgumentList "-NoExit -Command 'Write-Warning Looks like PowerExpressGUI ran into an error. You can use this terminal to see what went wrong, or close and continue your installation like normal.'";
 }
