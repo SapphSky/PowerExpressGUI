bf1551afb6c56b1890283d03345e5d85386f950c
Joel Fargas <joelfargas@revivn.com>
2024-07-08 09:03:22 -0400

    Hopefully better error detection

--- preboot.ps1
+++ preboot.ps1
@@ -1,16 +1,17 @@
 $DebugMode = $false;
 $ProgressTitle = "PowerExpressGUI Bootstrapper";
 
-if (-Not (Test-Path "C:\PowerExpressGUI\")) {
-    Write-Progress -Activity $ProgressTitle -Status "Creating directory";
-    New-Item -Path "C:\" -Name "PowerExpressGUI" -ItemType "directory";
-}
-
 # Download the autorun script
-$AutorunUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/content/driver-update.ps1";
-$AutorunFile = "C:\PowerExpressGUI\autorun.ps1";
-Invoke-RestMethod -Uri $AutorunUrl -OutFile $AutorunFile;
-Write-Progress -Activity $ProgressTitle -Status "Registering Scheduled Task";
+function DownloadScript {
+    if (-Not (Test-Path "C:\PowerExpressGUI\")) {
+        Write-Progress -Activity $ProgressTitle -Status "Creating directory";
+        New-Item -Path "C:\" -Name "PowerExpressGUI" -ItemType "directory";
+    }
+    $AutorunUrl = "https://github.com/SapphSky/PowerExpressGUI/raw/main/content/driver-update.ps1";
+    $AutorunFile = "C:\PowerExpressGUI\autorun.ps1";
+    Invoke-RestMethod -Uri $AutorunUrl -OutFile $AutorunFile;
+    Write-Progress -Activity $ProgressTitle -Status "Registering Scheduled Task";
+}
 
 # Creates a Scheduled Task to run our script at startup
 $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-Verb RunAs -NoExit -File $AutorunFile";
@@ -34,9 +35,10 @@ Register-ScheduledTask -TaskName $TaskName -Description $Description `
     -Principal $Principal `
     -Settings $Settings `
     -Trigger $Trigger `
-    -Force | Out-Host;
+    -Force;
 
 if (Get-ScheduledTask -TaskName $TaskName) {
+    DownloadScript;
     Write-Progress -Activity "PowerExpressGUI Bootstrapper" -Status "Completed.";
 }
 else {
@@ -47,5 +49,5 @@ else {
 Start-Sleep -Seconds 1;
 
 if ($DebugMode -eq $true) {
-    Start-Process "powershell" -Verb RunAs -Wait -NoExit;
+    Start-Process "powershell" -Verb RunAs -Wait -NoExit -Command "echo 'Looks like PowerExpressGUI ran into an error. You can use this Command Prompt to see what went wrong, or close and continue your installation like normal.'";
 }
