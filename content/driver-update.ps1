73a4188d4f669cff9f6d09b874a9aff2d2de2801
Joel Fargas <joelfargas@revivn.com>
2024-07-08 14:03:21 -0400

    Let's try it again

--- content/driver-update.ps1
+++ content/driver-update.ps1
@@ -1,22 +1,26 @@
-$MaxAttempts = 5;
+$CommandBlock = {
+    $MaxAttempts = 5;
 
-Write-Host 'Getting Package Provider'
-Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null;
-Write-Host 'Setting Repository'
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
+    Write-Host 'Getting Package Provider'
+    Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null;
+    Write-Host 'Setting Repository'
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
 
-for ($i = 1; $i -le $MaxAttempts; $i++) {
-    if (-Not (Get-Module -Name PSWindowsUpdate)) {
-        Write-Host 'Installing PSWindowsUpdate Module...';
-        Install-Module -Name PSWindowsUpdate -Force;
-        Start-Sleep -Seconds 1;
-        Write-Host 'Importing module...';
-        Import-Module -Name PSWindowsUpdate -Force;
+    for ($i = 1; $i -le $MaxAttempts; $i++) {
+        if (-Not (Get-Module -Name PSWindowsUpdate)) {
+            Write-Host 'Installing PSWindowsUpdate Module...';
+            Install-Module -Name PSWindowsUpdate -Force;
+            Start-Sleep -Seconds 1;
+            Write-Host 'Importing module...';
+            Import-Module -Name PSWindowsUpdate -Force;
+        }
+        else {
+            Write-Host "Checking for updates...";
+            Install-WindowsUpdate -AcceptAll -UpdateType Driver -AutoReboot;
+            break;
+        }
+        $i++;
     }
-    else {
-        Write-Host "Checking for updates...";
-        Install-WindowsUpdate -AcceptAll -UpdateType Driver -AutoReboot;
-        break;
-    }
-    $i++;
 }
+
+Start-Process powershell -Verb RunAs -Wait -ArgumentList "-WindowStyle Normal -Command {$CommandBlock}"
